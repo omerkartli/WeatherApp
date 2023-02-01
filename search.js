a1b6f2b2d5de81f9f42e2ee468afaa9e26858ee1
omerkartli <omerkartli52@gmail.com>
2023-02-01 11:15:52 +0300

    second screen links done

--- search.js
+++ search.js
@@ -11,7 +11,9 @@ function searchWeather() {
     .then(displayResult)
 };
 const displayResult = (results) => {
-  console.log(results);
+
+  console.log('display')
+  
   let result = results.sort((a, b) => (a.city > b.city) ? 1 : ((b.city > a.city) ? -1 : 0))//sort the cities
   console.log(result);
 
@@ -27,27 +29,35 @@ const displayResult = (results) => {
     return acc;
   }, []).sort();
 
-  console.log(output)
+    let listL = document.getElementById("myListOutput");
+    listL.innerHTML = "";
+  
+    for (let i=0; i<output.length; i++){
+      
+      let alphabet = document.createElement("li");
+      alphabet.innerText = output[i].alphabet[0];
+      alphabet.style.fontSize = "x-large";
+      alphabet.style.color = "grey"
+      alphabet.style.listStyleType = "none"
+      listL.appendChild(alphabet);
+      // console.log(output[i].alphabet[0])
 
-  for(let i=0; i<output.length; i++){
-    let alph = output[i].alphabet;
-    let listL = document.getElementById("myListL");
-    let liL = document.createElement("li");
-    liL.innerText = alph;
-    listL.appendChild(liL); 
-    
-    let b=output[i].cities.length
-    for(let j=0; j<b; j++){
-      let data = output[i].cities[j];
-      let list = document.getElementById("myList");
-      let li = document.createElement("li");
-      li.innerText = data;
-      list.appendChild(li);
-
+      for(let j=0; j<output[i].cities.length; j++){
+      
+        let citiess = document.createElement("li");
+        let link = document.createElement("a");
+        link.setAttribute("href", `index.html?city=${output[i].cities[j]}`)
+        link.innerText = output[i].cities[j];
+        citiess.appendChild(link)
+        listL.appendChild(citiess)
+        // citiess.appendChild(link).setAttribute("href", 'index.html');
+        // console.log(output[i].cities[j])
+      }
+      
     }
   
     
-  }
+  
 }
 
 
