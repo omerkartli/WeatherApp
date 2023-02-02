2a711df0c7fb4025a0c975d9a09fffd01fce93cd
omerkartli <omerkartli52@gmail.com>
2023-02-02 17:15:47 +0300

    firstly all function and csses done

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
 
 const API_URL = 'http://api.weatherapi.com/v1/'
-const key = 'a6d278e70e964ac4ac464022231801'
+const key = 'a528639a7c7242d5bb755022230202'
 
-const setQuery = (e) => {
-    if(e.keyCode =='13'){
-    getResult(searchBar.value)
-}}
-const getResult = (cityName) => {
-    // console.log(cityName);
-    let query = `${API_URL}forecast.json?q=${cityName}&days=7&key=${key}`
-    console.log(query)
-    fetch(query)
-    .then(weather => {
-        console.log(typeof(weather))
-        return weather.json()
-        
-    })
-    .then(displayResult)
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+console.log(params.city)
+
+
+
+document.getElementById('searchBar').onclick = function () {
+    console.log('clicked')
+     window.location.href = 'search.html';
 }
 
 const displayResult = (result) => {
+
     console.log(result);//jsona burdan bak
-    console.log(typeof(result))
+    console.log(typeof (result))
     let date = new Date();
     let date1 = document.querySelector('.Date')
-    date1.innerText = date.toLocaleString('en-US', { 
+    date1.innerText = date.toLocaleString('en-US', {
         weekday: 'long'
-    })+', '+date.toLocaleString('en-US', {
+    }) + ', ' + date.toLocaleString('en-US', {
         day: 'numeric'
-    })+' '+date.toLocaleString('en-US', {
-        month: 'short' 
-    })+' '+date.toLocaleString('en-US', {
+    }) + ' ' + date.toLocaleString('en-US', {
+        month: 'short'
+    }) + ' ' + date.toLocaleString('en-US', {
         year: 'numeric'
-    })+' | '+date.toLocaleString('en-US', {
+    }) + ' | ' + date.toLocaleString('en-US', {
         hour: 'numeric',
         minute: 'numeric'
     })
 
-    let loca = document.querySelector('.Location')//class adı location
-    loca.innerText = `${result.location.name}, ${result.location.country}`//jsondan kontrol et bak nerdec
-    
+    // let loca = document.querySelector('.Location')//class adı location
+    // loca.innerText = `${result.location.name}, ${result.location.country}`//jsondan kontrol et bak nerdec
+
 
     let iconimg = document.querySelector('.Iconimg')
     iconimg.src = `${(result.current.condition.icon)}`
@@ -54,10 +49,10 @@ const displayResult = (result) => {
 
     let maxarr = document.querySelector('.Max')
     maxarr.innerText = `${Math.round(result.forecast.forecastday[0].day.maxtemp_c)}°C`
-    
+
     let minarr = document.querySelector('.Min')
     minarr.innerText = `${Math.round(result.forecast.forecastday[0].day.mintemp_c)}°C`
-    
+
     let humi = document.querySelector('.Humidity')
     humi.innerText = `${result.current.humidity}%`
 
@@ -73,54 +68,68 @@ const displayResult = (result) => {
     let sunset = document.querySelector('.Sunset')
     sunset.innerText = `${result.forecast.forecastday[0].astro.sunset}`//bunu saat formatına çevir
 
-
-
-
     ////-----------DayTime----------------////////////
     const convertTime12to24h = (time12h) => {
         const [time, modifier] = time12h.split(' ');
-          
+
         let [hours, minutes] = time.split(':');
-          
+
         if (hours === '12') {
-              hours = '00';
+            hours = '00';
         }
-          
-        if (modifier === 'PM') {
-              hours = parseInt(hours, 10) + 12;
-        }
-          
-        return `${hours}`;
-        }
-    const convertTime12to24m = (time12h) => {
-        const [time, modifier] = time12h.split(' ');
-          
-        let [hours, minutes] = time.split(':');
-          
-        if (hours === '12') {
-              hours = '00';
-         }
-          
+
         if (modifier === 'PM') {
             hours = parseInt(hours, 10) + 12;
-        }  
-        return `${minutes}`;
         }
+
+        return `${hours}`;
+    }
+    const convertTime12to24m = (time12h) => {
+        const [time, modifier] = time12h.split(' ');
+
+        let [hours, minutes] = time.split(':');
+
+        if (hours === '12') {
+            hours = '00';
+        }
+
+        if (modifier === 'PM') {
+            hours = parseInt(hours, 10) + 12;
+        }
+        return `${minutes}`;
+    }
     let sunset24h = convertTime12to24h(`${result.forecast.forecastday[0].astro.sunset}`);
     let sunset24m = convertTime12to24m(`${result.forecast.forecastday[0].astro.sunset}`);
     let sunrise24h = convertTime12to24h(`${result.forecast.forecastday[0].astro.sunrise}`);
     let sunrise24m = convertTime12to24m(`${result.forecast.forecastday[0].astro.sunrise}`);
     let sunsettime = new Date(null, null, null, sunset24h, sunset24m);
     let sunrisetime = new Date(null, null, null, sunrise24h, sunrise24m);
-        
+
+
+    var now = new Date();
+    console.log(now.getHours())
+    console.log(sunrisetime.getHours())
+    console.log(sunsettime.getHours())
+    if (now.getHours() > sunrisetime.getHours() && now.getHours() < sunsettime.getHours()) {
+        let dayimg = document.querySelector('.day')
+        dayimg.src = "./images/graphicday.png"
+        document.body.className += "day";
+        console.log("sa")
+    } else {
+        let nightimg = document.querySelector('.day')
+        nightimg.src = "./images/graphicnight.png"
+        document.body.className += "night";
+        console.log("sa")
+    }
+
     let delta = Math.abs(sunrisetime - sunsettime) / 1000;
-    
+
     let hours = Math.floor(delta / 3600) % 24;
-        delta -= hours * 3600;
-    
+    delta -= hours * 3600;
+
     let minutes = Math.floor(delta / 60) % 60;
-        delta -= minutes * 60;
-    
+    delta -= minutes * 60;
+
     let daytime = document.querySelector('.Daytime') // Gün uzunluğunu dt emin değilim
     daytime.innerText = `${hours}h ${minutes}m`//bunu sunset-sunrise da yapılabilir
 
@@ -132,12 +141,12 @@ const displayResult = (result) => {
 
     date.setDate(date.getDate() + 1);
     let nextDay1 = document.querySelector('.NextDay1')
-    nextDay1.innerText = date.toLocaleString('en-US', { 
+    nextDay1.innerText = date.toLocaleString('en-US', {
         weekday: 'short'
-    })+', '+date.toLocaleString('en-US', {
+    }) + ', ' + date.toLocaleString('en-US', {
         day: 'numeric'
-    })+`\n ${Math.round(result.forecast.forecastday[1].day.maxtemp_c)}°C↑` +
-    `   ${Math.round(result.forecast.forecastday[1].day.mintemp_c)}°C↓`
+    }) + `\n ${Math.round(result.forecast.forecastday[1].day.maxtemp_c)}°C↑` +
+        `   ${Math.round(result.forecast.forecastday[1].day.mintemp_c)}°C↓`
 
 
     let iconimgnd2 = document.querySelector('.IconimgNd2')
@@ -145,12 +154,12 @@ const displayResult = (result) => {
 
     date.setDate(date.getDate() + 1);
     let nextDay2 = document.querySelector('.NextDay2')
-    nextDay2.innerText = date.toLocaleString('en-US', { 
+    nextDay2.innerText = date.toLocaleString('en-US', {
         weekday: 'short'
-    })+', '+date.toLocaleString('en-US', {
+    }) + ', ' + date.toLocaleString('en-US', {
         day: 'numeric'
-    })+`\n ${Math.round(result.forecast.forecastday[2].day.maxtemp_c)}°C↑` +
-    `   ${Math.round(result.forecast.forecastday[2].day.mintemp_c)}°C↓`
+    }) + `\n ${Math.round(result.forecast.forecastday[2].day.maxtemp_c)}°C↑` +
+        `   ${Math.round(result.forecast.forecastday[2].day.mintemp_c)}°C↓`
 
 
     let iconimgnd3 = document.querySelector('.IconimgNd3')
@@ -158,12 +167,12 @@ const displayResult = (result) => {
 
     date.setDate(date.getDate() + 1);
     let nextDay3 = document.querySelector('.NextDay3')
-    nextDay3.innerText = date.toLocaleString('en-US', { 
+    nextDay3.innerText = date.toLocaleString('en-US', {
         weekday: 'short'
-    })+', '+date.toLocaleString('en-US', {
+    }) + ', ' + date.toLocaleString('en-US', {
         day: 'numeric'
-    })+`\n ${Math.round(result.forecast.forecastday[3].day.maxtemp_c)}°C↑` +
-    `   ${Math.round(result.forecast.forecastday[3].day.mintemp_c)}°C↓`
+    }) + `\n ${Math.round(result.forecast.forecastday[3].day.maxtemp_c)}°C↑` +
+        `   ${Math.round(result.forecast.forecastday[3].day.mintemp_c)}°C↓`
 
 
     let iconimgnd4 = document.querySelector('.IconimgNd4')
@@ -171,12 +180,12 @@ const displayResult = (result) => {
 
     date.setDate(date.getDate() + 1);
     let nextDay4 = document.querySelector('.NextDay4')
-    nextDay4.innerText = date.toLocaleString('en-US', { 
+    nextDay4.innerText = date.toLocaleString('en-US', {
         weekday: 'short'
-    })+', '+date.toLocaleString('en-US', {
+    }) + ', ' + date.toLocaleString('en-US', {
         day: 'numeric'
-    })+`\n ${Math.round(result.forecast.forecastday[4].day.maxtemp_c)}°C↑` +
-    `   ${Math.round(result.forecast.forecastday[4].day.mintemp_c)}°C↓`
+    }) + `\n ${Math.round(result.forecast.forecastday[4].day.maxtemp_c)}°C↑` +
+        `   ${Math.round(result.forecast.forecastday[4].day.mintemp_c)}°C↓`
 
 
     let iconimgnd5 = document.querySelector('.IconimgNd5')
@@ -184,12 +193,12 @@ const displayResult = (result) => {
 
     date.setDate(date.getDate() + 1);
     let nextDay5 = document.querySelector('.NextDay5')
-    nextDay5.innerText = date.toLocaleString('en-US', { 
+    nextDay5.innerText = date.toLocaleString('en-US', {
         weekday: 'short'
-    })+', '+date.toLocaleString('en-US', {
+    }) + ', ' + date.toLocaleString('en-US', {
         day: 'numeric'
-    })+`\n ${Math.round(result.forecast.forecastday[5].day.maxtemp_c)}°C↑` +
-    `   ${Math.round(result.forecast.forecastday[5].day.mintemp_c)}°C↓`
+    }) + `\n ${Math.round(result.forecast.forecastday[5].day.maxtemp_c)}°C↑` +
+        `   ${Math.round(result.forecast.forecastday[5].day.mintemp_c)}°C↓`
 
 
     let iconimgnd6 = document.querySelector('.IconimgNd6')
@@ -197,21 +206,29 @@ const displayResult = (result) => {
 
     date.setDate(date.getDate() + 1);
     let nextDay6 = document.querySelector('.NextDay6')
-    nextDay6.innerText = date.toLocaleString('en-US', { 
+    nextDay6.innerText = date.toLocaleString('en-US', {
         weekday: 'short'
-    })+', '+date.toLocaleString('en-US', {
+    }) + ', ' + date.toLocaleString('en-US', {
         day: 'numeric'
-    })+`\n ${Math.round(result.forecast.forecastday[6].day.maxtemp_c)}°C↑` +
-    `   ${Math.round(result.forecast.forecastday[6].day.mintemp_c)}°C↓`
+    }) + `\n ${Math.round(result.forecast.forecastday[6].day.maxtemp_c)}°C↑` +
+        `   ${Math.round(result.forecast.forecastday[6].day.mintemp_c)}°C↓`
+}
 
-}     
+function getResult(cityName){
+    console.log(cityName);
+    let query = `${API_URL}forecast.json?q=${cityName}&days=7&key=${key}`
+    console.log(query)
+    fetch(query)
+        .then(weather => {
+            console.log(typeof (weather))
+            return weather.json()
+        })
+        .then(displayResult)
+}
 
+if (params.city) {
+    let searchBar = document.getElementById('searchBar')
+    searchBar.value = params.city;
+    this.getResult(params.city)
+}
 
-const searchBar=document.getElementById('searchBar')
-searchBar.addEventListener('keypress',setQuery)
-
-
-// function myFunction() {
-//     var x = document.getElementById("searchBar");
-//     x.value = x.value.toUpperCase();
-//   }
--- search.js
+++ search.js
@@ -1,33 +1,35 @@
-const API_URL = 'https://www.yahoo.com/news/_tdnews/api/resource/WeatherSearch;text'
+const API_URL2 = 'https://www.yahoo.com/news/_tdnews/api/resource/WeatherSearch;text'
 
 function searchWeather() {
 
   const searchTerm = document.getElementById("search-input").value;
-  if (searchTerm.length < 2) return;//if press enter 2 letter is without enter 3 letter is enough
-  let query = `${API_URL}=${searchTerm}`
+  if (searchTerm.length < 3) return;//if press enter 2 letter is without enter 3 letter is enough
+  let query = `${API_URL2}=${searchTerm}`
   console.log(query)
   fetch(query)
     .then(response => response.json())
-    .then(displayResult)
+    .then(displayResult2)
 };
-const displayResult = (results) => {
+const displayResult2 = (results) => {
 
   console.log('display')
   
   let result = results.sort((a, b) => (a.city > b.city) ? 1 : ((b.city > a.city) ? -1 : 0))//sort the cities
   console.log(result);
 
-  const output = [...result].reduce((acc, curr) => {
+  const output = [...result].reduce((acc, curr, i, k) => {
     const idx = acc.findIndex(e => e.alphabet === curr.city[0]);
     if (idx === -1) {
-      acc.push({ alphabet: curr.city[0], cities: [curr.city] });
+      acc.push({ alphabet: curr.city[0], cities: [`${curr.city}, ${curr.country}`] });
     }
     else {
-      acc[idx].cities.push(curr.city);
+      acc[idx].cities.push(`${curr.city}, ${curr.country}`);
       acc[idx].cities.sort((r1, r2) => r1.city > r2.city ? 1 : -1);
     }
     return acc;
   }, []).sort();
+
+  console.log(output)
 
     let listL = document.getElementById("myListOutput");
     listL.innerHTML = "";
@@ -36,22 +38,41 @@ const displayResult = (results) => {
       
       let alphabet = document.createElement("li");
       alphabet.innerText = output[i].alphabet[0];
-      alphabet.style.fontSize = "x-large";
-      alphabet.style.color = "grey"
+      alphabet.style.marginBottom = "17px"
+      alphabet.style.marginTop = "5px"
+      alphabet.style.fontSize = "40px";
+      alphabet.style.color = " #AAAAAA";
+      alphabet.style.fontFamily = "Barlow";
+      alphabet.style.fontStyle = "normal";
+      alphabet.style.fontWeight = 300;
+      alphabet.style.fontSize = "40px";
+      alphabet.style.lineHeight = "48px";
+      alphabet.style.letterSpacing = "-0.5em";
       alphabet.style.listStyleType = "none"
       listL.appendChild(alphabet);
-      // console.log(output[i].alphabet[0])
 
       for(let j=0; j<output[i].cities.length; j++){
       
         let citiess = document.createElement("li");
         let link = document.createElement("a");
-        link.setAttribute("href", `index.html?city=${output[i].cities[j]}`)
+        const first = output[i].cities[j].split(',')[0]//first value City name
+        // console.log(first);
+        link.setAttribute("href", `index.html?city=${first}`)
         link.innerText = output[i].cities[j];
         citiess.appendChild(link)
+        citiess.style.marginBottom = "5px"
+        citiess.style.listStyleType = "none";
+        link.style.textDecoration = "none";
+        link.style.textAnchor = "none";
+        link.style.color = "#444444"
+        citiess.style.textDecoration = "none";
+        link.style.fontFamily = 'Barlow';
+        link.style.fontStyle = "normal";
+        link.style.fontSize = "18px";
+        link.style.lineHeight = "22px";
+        link.style.fontWeight = "400";
+        link.style.letterSpacing = "-0.05em";
         listL.appendChild(citiess)
-        // citiess.appendChild(link).setAttribute("href", 'index.html');
-        // console.log(output[i].cities[j])
       }
       
     }
